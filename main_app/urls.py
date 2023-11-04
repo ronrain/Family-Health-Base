93b0e0649652fe47b888132bf967514d775d8ea2
RonRain <rarainero@gmail.com>
2023-11-04 15:02:01 -0400

    add index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('members/', views.member_index, name='member-index'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def member_index(request):
+  return render(request, 'members/index.html', { 'members': members })
