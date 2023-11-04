50f5ddeddd2b0dd0ee947ad5b7cb2fab463a181c
RonRain <rarainero@gmail.com>
2023-11-04 15:23:16 -0400

    add member model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+GENDER = (
+  ('M', 'Male'),
+  ('F', 'Female'),
+  ('N', 'Non-Binary'),
+  ('P', 'Prefer Not to Say')
+)
+
+class Member(models.Model):
+  name = models.CharField(max_length=100)
+  birth_date = models.DateField('Birth date')
+  gender = models.CharField(
+    max_length=1,
+    choices=GENDER
+  )
+  height = models.IntegerField()
+  weight = models.IntegerField()
+  medical_history = models.TextField(max_length=250)
+  update_date = models.DateField('Update date')
+
+  def __str__(self):
+    return self.name
