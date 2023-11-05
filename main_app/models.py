7436b12e984c640724ae3f1f8a56d263f7d7ab06
RonRain <rarainero@gmail.com>
2023-11-05 10:40:00 -0500

    add user to member model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 OPTIONS = (
   ('Yes', 'Yes'),
@@ -24,6 +25,7 @@ class Member(models.Model):
   weight = models.IntegerField()
   medical_history = models.TextField(max_length=250)
   update_date = models.DateField('Update Date')
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
